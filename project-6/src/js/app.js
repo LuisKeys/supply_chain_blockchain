558455795bdfe3d38759c9d316938c7a7005b452
LuisKeys <luis.paradela@gmail.com>
2022-07-15 10:43:27 -0300

    final version

--- project-6/src/js/app.js
+++ project-6/src/js/app.js
@@ -74,7 +74,7 @@ App = {
         }
         // If no injected web3 instance is detected, fall back to Ganache
         else {
-            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+            App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
         }
 
         App.getMetaskAccountID();
